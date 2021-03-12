c901614e626018e691105fdb657ee0ed6cfe6fca
Karthik Konkula <kkonkula1192@gmail.com>
2021-03-12 23:27:38 +0530

    added function signature

--- src/app/app.py
+++ src/app/app.py
@@ -4,6 +4,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def index() -> str:
     py = 'I Love Python'
     return py
+
+
+@app.route('/<message>')
+def echoMessage(message: str) -> str:
+    py = 'I Love Python from:'
+    return py + message
